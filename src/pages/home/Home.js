7a56b0a81d94a105a30ba63a4eb3540cfe473ccf
Aren Sinanian <95931889+arensin8@users.noreply.github.com>
2023-07-23 12:25:30 +0000

    Home Section in progress

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,9 @@
-import React from 'react';
 import './Home.scss'
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
+import Widget from '../../components/widget/Widget';
+import Chart from '../../components/chart/Chart';
+import Feautured from '../../components/feautured/Feautured';
 
 const Home = () => {
     return(
@@ -9,7 +11,16 @@ const Home = () => {
             <Sidebar />
             <div className='homeContainer'>
             <Navbar />
-                Home container
+            <div className='widgets'>
+                <Widget type="user"/>
+                <Widget type="order"/>
+                <Widget type="earning"/>
+                <Widget type="balance"/>
+            </div>
+            <div className='charts'>
+                <Feautured />
+                <Chart />
+            </div>
             </div>
         </div>
     )
